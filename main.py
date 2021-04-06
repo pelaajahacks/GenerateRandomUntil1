5f702069d9327f2fb7954f3863157c1f95a6e649
pelaajahacks <ronttipontti.sundstrom2@gmail.com>
2021-04-06 23:41:38 +0300

    Optimazition

--- main.py
+++ main.py
@@ -4,12 +4,22 @@ import time
 number_count = 0
 funny_numbers = []
 
+
+def print_time():
+    sec = secs2 % (24 * 3600)
+    hour = sec // 3600
+    sec %= 3600
+    minutes = sec // 60
+    sec %= 60
+    print(f"It took {hour} hours {minutes} minutes {secs:0.3f} seconds")
+
+
 start = time.perf_counter()
 
 print("The number is now loading...")
 
 while True:
-    number = random.randint(1, 999999)
+    number = random.randint(1, 9999999)
     if number == 1:
         finish = time.perf_counter()
         secs = finish - start
@@ -20,15 +30,12 @@ while True:
         print("Some data:")
         print("VVVVVV")
         print("it took", str(number_count), "tries")
-        sec = secs2 % (24 * 3600)
-        hour = sec // 3600
-        sec %= 3600
-        minutes = sec // 60
-        sec %= 60
-        print(f"It took {hour} hours {minutes} minutes {secs:0.2f} seconds")
+        print_time()
         print("1 digit numbers:", *funny_numbers)
         print("")
         print("How many 1 digit numbers:", len(funny_numbers))
+        print("Sleeping...")
+        time.sleep(60)
         break
     elif len(str(number)) == 1:
         funny_numbers.append(number)
